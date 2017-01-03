b679d9b14019f9100f596241acdbcaaec33d892c
agonzalez <Andres.Gonzalez@cyaninc.com>
2017-01-03 12:52:27 -0800

    Build adjustment - Added build artifacts to .gitignore - Tests have separate build configuration - Removed build artifacts from repo

--- dist/src/duck.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require("./actions"));
-var reducer_1 = require("./reducer");
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = reducer_1.default;
--- dist/tests/reducer-test.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var mocha_1 = require("mocha");
-mocha_1.describe('description', function () {
-    mocha_1.it('passes', function () { });
-});
